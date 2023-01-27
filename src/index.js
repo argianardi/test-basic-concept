96ab3832498bc2f9f55a0423aca9f9daddbed723
Argian Ardi Prasetya <argianardi1435@gmail.com>
2023-01-27 10:00:12 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const compression = require("compression");
+const helmet = require("helmet");
+const cors = require("cors");
+require("dotenv").config();
+
+//initialize express
+const app = express();
+
+// use packages
+app.use(cors());
+app.use(helmet());
+app.use(compression());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+// server listening
+const PORT = process.env.PORT || 6022;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
