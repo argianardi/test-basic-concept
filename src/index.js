c5cb399bb6703f62d1e00e1860ba7b06a90e428d
Argian Ardi Prasetya <argianardi1435@gmail.com>
2023-01-27 10:16:30 +0700

    add the answer to the first question (fibonacci)

--- src/index.js
+++ src/index.js
@@ -2,22 +2,43 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const compression = require("compression");
 const helmet = require("helmet");
-const cors = require("cors");
 require("dotenv").config();
 
 //initialize express
 const app = express();
 
 // use packages
-app.use(cors());
 app.use(helmet());
 app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // Routes
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get("/fibonaci", (req, res) => {
+  const { jumlahAngka } = req.body;
+
+  if (jumlahAngka <= 1) {
+    return n;
+  }
+
+  const result = [0, 1];
+
+  for (let i = 2; i <= jumlahAngka + 2; i++) {
+    result[i] = result[i - 2] + result[i - 1];
+  }
+
+  let fibonaciGanjil = [];
+  for (var i = 0; i < result.length; i++) {
+    if (result[i] % 2 !== 0) {
+      fibonaciGanjil.push(result[i]);
+    }
+  }
+  fibonaciGanjil.reverse();
+
+  res.status(200).json({
+    succes: "true",
+    data: fibonaciGanjil,
+  });
 });
 
 // server listening
