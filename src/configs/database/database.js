f0f15246da30b22bd9faa18d78c10cdcf8173bff
Argian Ardi Prasetya <argianardi1435@gmail.com>
2023-01-27 23:04:04 +0700

    configure database

---
+++ src/configs/database/database.js
@@ -0,0 +1,23 @@
+const Sequelize = require("sequelize");
+
+const db = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
+  {
+    dialect: "mysql",
+    host: process.env.DB_HOST,
+  }
+);
+
+db.authenticate()
+  .then(() => {
+    console.log(
+      "Connection to the database has been established successfully."
+    );
+  })
+  .catch((error) => {
+    console.error("Unable to connect to the database: ", error.message);
+  });
+
+module.exports = db;
